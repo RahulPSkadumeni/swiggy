566af3875ff6a38b4851f859add68d59745853b6
Akshay Saini <akshaymarch7@gmail.com>
2022-12-31 14:47:04 +0530

    session-2: Igniting our App

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
+/***
+ *
+ * Created A Server
+ * HMR - Hot Module Replacement
+ * File Watcher algorithm - C++
+ * BUNDLING
+ * MINIFY
+ * Cleaning our Code
+ * Dev abd Production Build
+ * Super Fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatble with older version of browser
+ * HTTPS on dev
+ * port Number
+ * Consistent Hashing Algorithm
+ * Zero Config
+ *
+ *
+ *
+ * Transitive Dependencies
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   {
     id: "title",
   },
-  "Heading 1"
+  "Heading 1 for parcel"
 );
 
 const heading2 = React.createElement(
@@ -18,6 +45,7 @@ const container = React.createElement(
   "div",
   {
     id: "container",
+    hello: "world",
   },
   [heading, heading2]
 );
