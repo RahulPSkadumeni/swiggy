5132c71fde59afb4abc972f88f51b6f3f8bef765
Akshay Saini <akshaymarch7@gmail.com>
2023-01-14 14:11:45 +0530

    chapter-06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,51 @@
 import { restaurantList } from "../contants";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-
-// What is state
-// what is React Hooks? - functions,
-// What is useState
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 function filterData(searchText, restaurants) {
+  // 8 restraunt list = > filtered  rest with "King"
   const filterData = restaurants.filter((restaurant) =>
-    restaurant.data.name.includes(searchText)
+    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
   );
 
   return filterData;
 }
 
 const Body = () => {
-  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [searchText, setSearchText] = useState("");
 
-  return (
+  // empty dependency array => once after render
+  // dep arry [searchText] => once after initial render + everytime after redern (my searchText changes)
+  useEffect(() => {
+    // API call
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    // Optional Chaining
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  console.log("render");
+
+  // not render component (Early return)
+  if (!allRestaurants) return null;
+
+  if (filteredRestaurants?.length === 0)
+    return <h1>No Restraunt match your Filter!!</h1>;
+
+  return allRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="search-container">
         <input
@@ -34,16 +61,17 @@ const Body = () => {
           className="search-btn"
           onClick={() => {
             //need to filter the data
-            const data = filterData(searchText, restaurants);
+            const data = filterData(searchText, allRestaurants);
             // update the state - restaurants
-            setRestaurants(data);
+            setFilteredRestaurants(data);
           }}
         >
           Search
         </button>
       </div>
       <div className="restaurant-list">
-        {restaurants.map((restaurant) => {
+        {/* You have to write logic for NO restraunt fount here */}
+        {filteredRestaurants.map((restaurant) => {
           return (
             <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
           );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,10 @@
+import { useState } from "react";
+
+const loggedInUser = () => {
+  // API call to check authentication
+  return false;
+};
+
 const Title = () => (
   <a href="/">
     <img
@@ -9,10 +16,11 @@ const Title = () => (
 );
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="header">
       <Title />
-
       <div className="nav-items">
         <ul>
           <li>Home</li>
@@ -21,6 +29,11 @@ const Header = () => {
           <li>Cart</li>
         </ul>
       </div>
+      {isLoggedIn ? (
+        <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+      ) : (
+        <button onClick={() => setIsLoggedIn(true)}>Login</button>
+      )}
     </div>
   );
 };
