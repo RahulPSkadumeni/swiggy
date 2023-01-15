a1e0a48ea31065dc6463974df4b5b5b5a2829c17
Akshay Saini <akshaymarch7@gmail.com>
2023-01-15 13:58:44 +0530

    chapter-07

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,9 +2,9 @@ import { restaurantList } from "../contants";
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
 
 function filterData(searchText, restaurants) {
-  // 8 restraunt list = > filtered  rest with "King"
   const filterData = restaurants.filter((restaurant) =>
     restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
   );
@@ -17,10 +17,7 @@ const Body = () => {
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [searchText, setSearchText] = useState("");
 
-  // empty dependency array => once after render
-  // dep arry [searchText] => once after initial render + everytime after redern (my searchText changes)
   useEffect(() => {
-    // API call
     getRestaurants();
   }, []);
 
@@ -29,19 +26,12 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    console.log(json);
-    // Optional Chaining
     setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
     setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
   }
 
-  console.log("render");
-
   // not render component (Early return)
   if (!allRestaurants) return null;
-
-  if (filteredRestaurants?.length === 0)
-    return <h1>No Restraunt match your Filter!!</h1>;
 
   return allRestaurants?.length === 0 ? (
     <Shimmer />
@@ -73,7 +63,12 @@ const Body = () => {
         {/* You have to write logic for NO restraunt fount here */}
         {filteredRestaurants.map((restaurant) => {
           return (
-            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+            <Link
+              to={"/restaurant/" + restaurant.data.id}
+              key={restaurant.data.id}
+            >
+              <RestaurantCard {...restaurant.data} />
+            </Link>
           );
         })}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,13 @@
 import { useState } from "react";
+import Logo from "../assets/img/foodvilla.png";
+import { Link } from "react-router-dom";
 
-const loggedInUser = () => {
-  // API call to check authentication
-  return false;
-};
+// SPA - Single Page Application???
+// Client Side Routing
 
 const Title = () => (
   <a href="/">
-    <img
-      className="logo"
-      alt="logo"
-      src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
-    />
+    <img className="logo" alt="logo" src={Logo} />
   </a>
 );
 
@@ -23,9 +19,16 @@ const Header = () => {
       <Title />
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contact</li>
+          </Link>
           <li>Cart</li>
         </ul>
       </div>
